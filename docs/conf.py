67600456bc4b9f27cd5f8f37f5098554f4a56ecc
David Petzel <davidpetzel@gmail.com>
2012-02-18 11:14:10 -0500

    still trying to fix the abbreviated title

--- docs/conf.py
+++ docs/conf.py
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'zenosszcautils', u'Zenoss ZCA Utilities Documentation',
+    ('index', 'zenosszcautilitiess', u'Zenoss ZCA Utilities Documentation',
      [u'ZCA'], 1)
 ]
 
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ZenossZCAUtils', u'Zenoss ZCA Utilities Documentation',
-   u'ZCA', 'ZenossZCAUtils', 'One line description of project.',
+  ('index', 'ZenossZCAUtilitiess', u'Zenoss ZCA Utilities Documentation',
+   u'ZCA', 'ZenossZCAUtilitiess', 'One line description of project.',
    'Miscellaneous'),
 ]
 
