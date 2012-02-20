edc6439cd78bbc045918f94d2ade5f054ffdbabc
dpetzel <davidpetzel@gmail.com>
2012-02-19 23:16:33 -0500

    dropping build number again as 1440 is also missing zenoss bits

--- source/scripts/vmware_appliance_to_virtualbox/vmwware_appliance_to_virtualbox.sh
+++ source/scripts/vmware_appliance_to_virtualbox/vmwware_appliance_to_virtualbox.sh
@@ -9,7 +9,7 @@
 
 # Some Default Variables
 # @todo - Enhance this to take command line arguments for more flexibility
-buildNumber="4.1.70-1440"
+buildNumber="4.1.70-1439"
 arch="x86_64"
 baseFileName="zenoss-$buildNumber-$arch"
 zipFileName="$baseFileName.vmware.zip"
