53800a8ae383779815809a9995fd2e0352cfe679
dpetzel <davidpetzel@gmail.com>
2012-03-22 16:27:17 -0300

    updating x64 rpm file name as mysql no longer makes the old one available

--- source/scripts/4x_alpha_builder/4x_alpha_build.sh
+++ source/scripts/4x_alpha_builder/4x_alpha_build.sh
@@ -39,9 +39,9 @@ fi
 # translate clearly. So just using if with a little duplication
 if [ "$arch" = "x86_64" ];then
 	jre_file="jre-6u31-linux-x64-rpm.bin"
-	mysql_client_rpm="MySQL-client-5.5.20-1.linux2.6.x86_64.rpm"
-	mysql_server_rpm="MySQL-server-5.5.20-1.linux2.6.x86_64.rpm"
-	mysql_shared_rpm="MySQL-shared-5.5.20-1.linux2.6.x86_64.rpm"
+	mysql_client_rpm="MySQL-client-5.5.21-1.linux2.6.x86_64.rpm"
+	mysql_server_rpm="MySQL-server-5.5.21-1.linux2.6.x86_64.rpm"
+	mysql_shared_rpm="MySQL-shared-5.5.21-1.linux2.6.x86_64.rpm"
 	rpmforge_rpm_file="rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm"
 	
 elif [ "$arch" = "i386" ]; then
