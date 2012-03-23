67e30f28f6a205c64c706744eaa10582fb4a5f90
David Petzel <davidpetzel@gmail.com>
2012-03-22 22:26:19 -0400

    updates to do some validation checks before getting to far along

--- source/scripts/4x_alpha_builder/4x_alpha_build.sh
+++ source/scripts/4x_alpha_builder/4x_alpha_build.sh
@@ -4,7 +4,8 @@
 # A silly little script to make installing a 
 # Zenoss Core 4.x alpha development/testing machine
 # to save me a little time next time a new release is cut
-# VERY Centos 5 centric/dependant
+# VERY Centos 5 centric/dependant.
+# Its assumed you are running this on a bare/clean machine
 #
 #
 ###################################################
@@ -31,13 +32,31 @@ else
 	zenoss_build=$2
 fi
 
+echo "Ensuring This server is in a clean state before we start"
+mysql_installed=0
+if [ `rpm -qa | grep -c -i mysql` -gt 0 ]; then
+	if [ `rpm -qa | grep -i mysql | grep -c -v 5.5` -gt 0 ]; then
+		echo "It appears you already have an older version of MySQL Install"
+		echo "I'm to scared to continue. Please remove existing MySQL Packages"
+		exit 1
+	else
+		echo "It appears MySQL 5.5 is already installed. MySQL Installation  will be skipped"
+		mysql_installed=1
+	fi
+fi
+
+echo "Ensuring Zenoss RPMs are not already present"
+if [ `rpm -qa | grep -c -i zenoss` -gt 0 ]; then
+	echo "I see Zenoss Packages already installed. I can't handle that"
+	exit 1
+fi
 
 
 
 # Where to get stuff. Base decisions on arch. Originally I was going to just
 # use the arch variable, but its a little dicey in that file names don't always
 # translate clearly. So just using if with a little duplication
-if [ "$arch" = "x86_64" ];then
+if [ "$arch" = "x86_64" ]; then
 	jre_file="jre-6u31-linux-x64-rpm.bin"
 	mysql_client_rpm="MySQL-client-5.5.21-1.linux2.6.x86_64.rpm"
 	mysql_server_rpm="MySQL-server-5.5.21-1.linux2.6.x86_64.rpm"
@@ -55,30 +74,33 @@ else
 	exit 1
 fi
 
-jre_download="http://download.oracle.com/otn-pub/java/jdk/6u31-b04/$jre_file"
-
-zenoss_arch=$arch
-zenoss_rpm_file="zenoss-$zenoss_build.el5.$zenoss_arch.rpm"
-zenpack_rpm_file="zenoss-core-zenpacks-$zenoss_build.el5.$zenoss_arch.rpm"
-zenoss_base_url="http://downloads.sourceforge.net/project/zenoss/zenoss-alpha/$zenoss_build"
-zenoss_gpg_key="http://dev.zenoss.org/yum/RPM-GPG-KEY-zenoss"
-
 rpm_forge_rpm_url="http://pkgs.repoforge.org/rpmforge-release/$rpmforge_rpm_file"
 
 
 cd /tmp
 
 echo "Downloading Files"
-if [ ! -f $jre_file ];then
-	echo "Downloading Oracle JRE"
-	wget $jre_download
-	chmod +x $jre_file
+if [ `rpm -qa | grep -c -i jre` -eq 0 ]; then
+	if [ ! -f $jre_file ];then
+		jre_download="http://download.oracle.com/otn-pub/java/jdk/6u31-b04/$jre_file"
+		echo "Downloading Oracle JRE"
+		wget $jre_download
+		chmod +x $jre_file
+	fi
+	if [ `rpm -qa | grep -c jre` -eq 0 ]; then
+		echo "Installating JRE"
+		./$jre_file
+	fi
+else
+	echo "Appears you already have a JRE installed. I'm not going to install another one"
 fi
-if [ `rpm -qa | grep -c jre` -eq 0 ];then
-	echo "Installating JRE"
-	./$jre_file
-fi
+
 echo "Downloading Zenoss RPMs"
+zenoss_arch=$arch
+zenoss_rpm_file="zenoss-$zenoss_build.el5.$zenoss_arch.rpm"
+zenpack_rpm_file="zenoss-core-zenpacks-$zenoss_build.el5.$zenoss_arch.rpm"
+zenoss_base_url="http://downloads.sourceforge.net/project/zenoss/zenoss-alpha/$zenoss_build"
+zenoss_gpg_key="http://dev.zenoss.org/yum/RPM-GPG-KEY-zenoss"
 for file in $zenoss_rpm_file $zenpack_rpm_file;do
 	if [ ! -f $file ];then
 		wget $zenoss_base_url/$file
@@ -91,16 +113,19 @@ if [ `rpm -qa gpg-pubkey* | grep -c "aa5a1ad7-4829c08a"` -eq 0  ];then
 	rpm --import $zenoss_gpg_key
 fi
 
-for file in $mysql_client_rpm $mysql_server_rpm $mysql_shared_rpm;
-do
-	if [ ! -f $file ];then
-		wget http://dev.mysql.com/get/Downloads/MySQL-5.5/$file/from/http://mirror.services.wisc.edu/mysql/
-	fi
-	rpm_entry=`echo $file | sed s/.x86_64.rpm//g | sed s/.i386.rpm//g | sed s/.i586.rpm//g`
-	if [ `rpm -qa | grep -c $rpm_entry` -eq 0 ];then
-		rpm -ivh $file
-	fi
-done
+if [ $mysql_installed -eq 0 ]; then
+	#Only install if MySQL Is not already installed
+	for file in $mysql_client_rpm $mysql_server_rpm $mysql_shared_rpm;
+	do
+		if [ ! -f $file ];then
+			wget http://dev.mysql.com/get/Downloads/MySQL-5.5/$file/from/http://mirror.services.wisc.edu/mysql/
+		fi
+		rpm_entry=`echo $file | sed s/.x86_64.rpm//g | sed s/.i386.rpm//g | sed s/.i586.rpm//g`
+		if [ `rpm -qa | grep -c $rpm_entry` -eq 0 ];then
+			rpm -ivh $file
+		fi
+	done
+fi
 
 echo "Installing Zenoss Dependency Repo"
 rpm -ivh http://deps.zenoss.com/yum/zenossdeps.el5.noarch.rpm
@@ -129,14 +154,15 @@ echo "Installing Core ZenPacks"
 rpm -ivh $zenpack_rpm_file
 
 
+echo "Please remember you can use the new zenpack --fetch command to install most zenpacks into your new core 4 Alpha install"
 #If your working with alpha, odds are you are going to need some zenpacks from source.
 #lets install the git client
-echo "Installing GIT client from RPMFORGE"
-if [ `rpm -qa | grep -c -i git` -eq 0 ];then
-	if [ `rpm -qa | grep -c -i rpmforge` -eq 0 ];then
-		wget -N $rpm_forge_rpm_url
-		rpm -ivh $rpmforge_rpm_file
-	fi
-	yum -y install git
-fi
+#echo "Installing GIT client from RPMFORGE"
+#if [ `rpm -qa | grep -c -i git` -eq 0 ];then
+#	if [ `rpm -qa | grep -c -i rpmforge` -eq 0 ];then
+#		wget -N $rpm_forge_rpm_url
+#		rpm -ivh $rpmforge_rpm_file
+#	fi
+#	yum -y install git
+#fi
 
